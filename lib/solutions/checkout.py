246d5bf108a75218a26ddb5e4df99c2637751d4c
sbx_user1059 <sbx_user1059@ip-10-11-134-114.eu-west-2.compute.internal>
2018-03-09 02:36:01 +0000

    Fri Mar 09 02:36:01 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -32,15 +32,15 @@ PRICES = [
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    price = -1
     sku_list = list(skus.upper())
     if len(sku_list) > 0:
         denomination = Counter(sku_list)
+        total_price = 0
         for key, value in denomination.items():
             price_obj = [price for price in PRICES if price['item'] == key]
             if len(price_obj) > 0:
                 obj = price_obj[0]
-                item_price = obj.get('price')
+                individual_price = obj.get('price')
                 offer = obj.get('offer')
                 if offer:  # check if there is a special offer
                     quantity = offer.get('quantity')
@@ -48,14 +48,15 @@ def checkout(skus):
                     remainder = value % quantity
                     offered_quantity = value / quantity
                     if remainder == 0:
-                        price = offered_quantity * special_price
+                        item_price = offered_quantity * special_price
                     else:
                         # check if special offer applies
                         if offered_quantity > 0:  # Apply special offer
                             numbered_quantity = value - remainder
-                            price = numbered_quantity * special_price
-                            remainder_price = remainder * item_price
-                            price += remainder_price
+                            item_price = numbered_quantity * special_price
+                            remainder_price = remainder * individual_price
+                            item_price += remainder_price
                         else:
-                            price = remainder * item_price
-    return price
+                            item_price = remainder * individual_price
+                    total_price += item_price
+    return -1
