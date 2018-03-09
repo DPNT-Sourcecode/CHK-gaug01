22c828dc12bf019caedde87921864f54c7b03dbe
sbx_user1059 <sbx_user1059@ip-10-11-134-114.eu-west-2.compute.internal>
2018-03-09 02:09:01 +0000

    Fri Mar 09 02:09:01 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,40 @@
+from collections import Counter
 
+prices = [
+    {
+        "item": "A",
+        "price": 50,
+        "offer": {  # used for special offer
+            "quantity": 3,
+            "special_price": 130
+        }
+    },
+    {
+        "item": "B",
+        "price": 30,
+        "offer": {  # used for special offer
+            "quantity": 2,
+            "special_price": 45
+        }
+    },
+    {
+        "item": "C",
+        "price": 20,
+        "offer": {}
+    },
+    {
+        "item": "D",
+        "price": 15,
+        "offer": {}
+    },
+]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    sku_list = list(skus)
+    if len(sku_list) > 0:
+        denomination = Counter(sku_list)
+        for key, value in denomination.items():
+            
+    return -1
