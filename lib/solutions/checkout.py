d3082c48ad4d7bd837858293ad498984fb78b726
sbx_user1059 <sbx_user1059@ip-10-12-182-153.eu-west-2.compute.internal>
2018-03-09 15:31:53 +0000

    Fri Mar 09 15:31:53 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -265,6 +265,8 @@ def checkout(skus):
 
             num_group = promo_len / GROUP_PROMO['quantity']
             total_price += (GROUP_PROMO['special_price'] * num_group)
+            print(total_price)
+            print(PROMO_COMBINATIONS)
 
         denomination = Counter(sku_list)
         keys = set(denomination.keys())
