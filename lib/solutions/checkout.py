a58a26a6f639dc3c90a805475bc360ff7e844fb4
sbx_user1059 <sbx_user1059@ip-10-11-134-114.eu-west-2.compute.internal>
2018-03-09 02:30:01 +0000

    Fri Mar 09 02:30:01 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-prices = [
+PRICES = [
     {
         "item": "A",
         "price": 50,
@@ -32,9 +32,30 @@ prices = [
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    price = -1
     sku_list = list(skus)
     if len(sku_list) > 0:
         denomination = Counter(sku_list)
         for key, value in denomination.items():
-            
-    return -1
+            price_obj = [price for price in PRICES if price['item'] == key]
+            if len(price_obj) > 0:
+                obj = price_obj[0]
+                item_price = obj.get('price')
+                offer = obj.get('offer')
+                if offer:  # check if there is a special offer
+                    quantity = offer.get('quantity')
+                    special_price = offer.get('special_price')
+                    remainder = value % quantity
+                    offered_quantity = value / quantity
+                    if remainder == 0:
+                        price = offered_quantity * special_price
+                    else:
+                        # check if special offer applies
+                        if offered_quantity > 0:  # Apply special offer
+                            numbered_quantity = value - remainder
+                            price = numbered_quantity * special_price
+                            remainder_price = remainder * item_price
+                            price += remainder_price
+                        else:
+                            price = remainder * item_price
+    return price
