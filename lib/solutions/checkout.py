4b1c54b3eaf7dbe50965dc7018908c4233c7419b
sbx_user1059 <sbx_user1059@ip-10-11-134-114.eu-west-2.compute.internal>
2018-03-09 02:45:01 +0000

    Fri Mar 09 02:45:01 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -37,31 +37,36 @@ def checkout(skus):
     if len(sku_list) > 0:
         denomination = Counter(sku_list)
         total_price = 0
-        for key, value in denomination.items():
-            price_obj = [price for price in PRICES if price['item'] == key]
-            if len(price_obj) > 0:
-                item_price = 0
-                obj = price_obj[0]
-                individual_price = obj.get('price')
-                offer = obj.get('offer')
-                if offer:  # check if there is a special offer
-                    quantity = offer.get('quantity')
-                    special_price = offer.get('special_price')
-                    remainder = value % quantity
-                    offered_quantity = value / quantity
-                    if remainder == 0:
-                        item_price = offered_quantity * special_price
-                    else:
-                        # check if special offer applies
-                        if offered_quantity > 0:  # Apply special offer
-                            numbered_quantity = value - remainder
-                            item_price = numbered_quantity * special_price
-                            remainder_price = remainder * individual_price
-                            item_price += remainder_price
-                        else:  # special offer not applicable
-                            item_price = remainder * individual_price
-                else:  # No special offer
-                    item_price = value * individual_price
-                total_price += item_price
-        return total_price
+        keys = set(denomination.keys())
+        item_keys = set(ITEMS)
+        if keys.intersection(item_keys):
+            for key, value in denomination.items():
+                price_obj = [price for price in PRICES if price['item'] == key]
+                if len(price_obj) > 0:
+                    item_price = 0
+                    obj = price_obj[0]
+                    individual_price = obj.get('price')
+                    offer = obj.get('offer')
+                    if offer:  # check if there is a special offer
+                        quantity = offer.get('quantity')
+                        special_price = offer.get('special_price')
+                        remainder = value % quantity
+                        offered_quantity = value / quantity
+                        if remainder == 0:
+                            item_price = offered_quantity * special_price
+                        else:
+                            # check if special offer applies
+                            if offered_quantity > 0:  # Apply special offer
+                                numbered_quantity = value - remainder
+                                item_price = numbered_quantity * special_price
+                                remainder_price = remainder * individual_price
+                                item_price += remainder_price
+                            else:  # special offer not applicable
+                                item_price = remainder * individual_price
+                    else:  # No special offer
+                        item_price = value * individual_price
+                    total_price += item_price
+            return total_price
+        else:
+            return -1
     return -1
